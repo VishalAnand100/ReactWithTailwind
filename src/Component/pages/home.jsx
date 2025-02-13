27a0d4a4ac27bc153b4e7740c2e535fdebe13fbc
VishalAnand100 <vishal.anand@simplifyingskills.com>
2025-02-13 17:29:55 +0530

    create multiple section

---
+++ src/Component/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Nav from "../nav"
+import Footer from "../footer"
+
+const Home=function(){
+return (
+    <div>
+    <Nav></Nav>
+    <h>Home</h>
+    <Footer></Footer>
+    </div>
+)
+}
+export default Home
